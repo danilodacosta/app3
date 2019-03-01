6b6d6373206e89640bd3103c910df03a4a02c187
Danilo Pereira <danilo-gc@hotmail.com>
2019-03-01 15:17:25 -0300

    autenticacao com usuario utilizando o firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app3';
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  ngOnInit(): void {
+
+    const config = {
+      apiKey: 'AIzaSyCH76knSiZvx-sqAMkGe_Pl6Rw1P5NqjWs',
+      authDomain: 'angular-instagram-clone-70b72.firebaseapp.com',
+      databaseURL: 'https://angular-instagram-clone-70b72.firebaseio.com',
+      projectId: 'angular-instagram-clone-70b72',
+      storageBucket: 'angular-instagram-clone-70b72.appspot.com',
+      messagingSenderId: '87627455973'
+    };
+
+     firebase.initializeApp(config);
+  }
 }
--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,17 @@
 import { Usuario } from './acesso/usuario.model';
+import * as firebase from 'firebase';
 
 export class Autenticacao {
-    public cadastrarUsuario(usuario: Usuario): void {
-        console.log('usuario cadastro com sucesso ! : ', usuario);
-    }
+  public cadastrarUsuario(usuario: Usuario): void {
+    console.log('usuario cadastro com sucesso ! : ', usuario);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+      .then((resposta: any) => {
+        console.log(resposta);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+      });
+  }
 }
